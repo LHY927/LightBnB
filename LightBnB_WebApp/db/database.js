a0331a772059b04f35666c55cf6378c2096002b3
LHY927 <liuhaiyue0927@gmail.com>
2024-02-26 02:01:21 -0500

    refactored getAllProperties function to accept different options

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -110,8 +110,53 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
+  
+  const queryParams = [];
+  
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // filters for city, owner_id, price
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += addClause(queryParams, `city LIKE $${queryParams.length}`);
+    //queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+  if (options.owner_id){
+    queryParams.push(options.owner_id);
+    queryString += addClause(queryParams, `owner_id = $${queryParams.length}`);
+    //queryString += `WHERE owner_id = $${queryParams.length}`;
+  }
+  if (options.minimum_price_per_night && options.maximum_price_per_night){
+    queryParams.push(options.minimum_price_per_night*100, options.maximum_price_per_night*100);
+    queryString += addClause(queryParams, `cost_per_night > $${queryParams.length -1} AND cost_per_night < $${queryParams.length}`);
+    //queryString += `WHERE cost_per_night > $${queryParams.length -1} AND cost_per_night < $${queryParams.length}`
+  }
+
+  // GROUP BY
+  queryString += `
+  GROUP BY properties.id
+  `;
+  // HAVING after GROUP BY
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING AVG(property_reviews.rating) > $${queryParams.length}`;
+  }
+  
+  //Other clauses
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
       return result.rows;
     })
@@ -119,6 +164,15 @@ const getAllProperties = (options, limit = 10) => {
       console.log(err.message);
     });
 };
+
+const addClause = function(arr, str){
+  if(arr.length === 1){
+    return 'WHERE ' + str;
+  }else if(arr.length > 1){
+    return ' AND ' + str;
+  }
+  return '';
+}
 
 /**
  * Add a property to the database
